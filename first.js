2027d7d6e0663d1731323d7bfd16cb8ee46db80c
Duzie Uche-Abba <duzzi4x@gmail.com>
2022-05-25 20:52:42 +0200

    tax round-up logic added

--- first.js
+++ first.js
@@ -4,7 +4,7 @@ let basicTax = 10;
 let importDuty = 5;
 
 //imported basic taxable items attract an extra non-negotiable tax rate of 5%, therefore 10% + 5% = 15%
-let basicPlusImportDuty = basicTax + importDuty; 
+let basicPlusImportDuty = basicTax + importDuty;
 
 //An app that computes the first basket of goods
 const basketOne = () => {
@@ -14,9 +14,9 @@ const basketOne = () => {
 
   // The plus sign is used in front of every variable to indicate to JavaScript that they are numerical values
   // Without this, they would become concatenated strings
-  
-    //these are the sums of the items and their taxes
-  let bookPlusTax = +taxFree * +book + +book; 
+
+  //these are the sums of the items and their taxes
+  let bookPlusTax = +taxFree * +book + +book;
   let musicCDPlusTax = (basicTax * +musicCD) / 100 + +musicCD;
   let chocolateBarPlusTax = +taxFree * +chocolateBar + +chocolateBar;
 
@@ -26,6 +26,13 @@ const basketOne = () => {
 
   //this computes the total sales tax generated
   let totalTaxes =
- ( bookPlusTax - book) + (musicCDPlusTax - musicCD) + (chocolateBarPlusTax - chocolateBar);
-  document.getElementById("sales-taxes").value = totalTaxes.toFixed(2);
+    bookPlusTax -
+    book +
+    (musicCDPlusTax - musicCD) +
+    (chocolateBarPlusTax - chocolateBar);
+
+  //Math.ceil and toFixed method helps us round up to the nearest 0.05
+  document.getElementById("sales-taxes").value = (
+    Math.ceil(totalTaxes * 20) / 20
+  ).toFixed(2);
 };
